778777087c5bdfad84228666e210c26fc479b508
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-01-25 17:21:38 +0100

    print array on screen

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Php Hotels</title>
 </head>
 
@@ -52,7 +53,7 @@
     ];
 
     ?>
-    <h1>php hotels list: </h1>
+    <h1 class="text-warning">php hotels list: </h1>
 
     <?php
 
